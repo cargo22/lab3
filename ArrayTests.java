8b6415db68600159570ce22e126de401f24e30e0
cargo22 <carlosegomez1738@gmail.com>
2023-04-20 16:46:07 -0700

    Fixed Bugs

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,23 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversedMultipleElems() {
+    int[] input3 = { 1 , 2 , 3 , 4 , 5 };
+    ArrayExamples.reverseInPlace(input3);
+    assertArrayEquals(new int[]{ 5 , 4 , 3 , 2 , 1 }, input3);
+  }
+
+  @Test
+  public void testReversedTwo() {
+    int[] input4 = { 1 , 2 };
+    assertArrayEquals(new int[]{ 2 , 1 }, ArrayExamples.reversed(input4));
+  }
+
+  @Test
+  public void testAverageWithoutLowest() {
+    double[] input4 = { 3 , 3 , 3 };
+    assertEquals(3, ArrayExamples.averageWithoutLowest(input4), 0.0);
+  }
 }
