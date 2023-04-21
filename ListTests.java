3dcefad3c1ba3fc27e514006ca5aedac66c59189
cargo22 <carlosegomez1738@gmail.com>
2023-04-20 17:44:43 -0700

    Update ListTests.java

--- ListTests.java
+++ ListTests.java
@@ -14,4 +14,15 @@ public class ListTests {
         StringChecker sc = new ListExamples();
         assertEquals(test, ListExamples.filter(result, sc));
 	} 
+
+    @Test 
+	public void testListMerge(){
+        String[] inp = {"a", "c", "e", "g"};
+        List<String> result = Arrays.asList(inp);
+        String[]inp2 = {"b","d","f","h"};
+        List<String> test = Arrays.asList(inp2);
+        String[] res = {"a", "b", "c", "d", "e", "f", "g" , "h"};
+        List<String> asdf = Arrays.asList(res);
+        assertEquals(asdf, ListExamples.merge(result, test));
+	} 
 }
